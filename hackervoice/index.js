7e9e2c721f8089fc4cbfbb5e220843a6a5c479fb
Shi Tao Luo <sluo5991@gmail.com>
2022-06-13 19:53:40 -0400

    fix #2

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -1,13 +1,13 @@
 module.exports = async function (context, req) {
-  context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
 
   // param name = password
   var password = req.query.password;
   context.log(password);
 
-  let result = '';
+  let result = "";
 
-  result = password == 'letmein' ? 'Access granted.' : 'Access denied.';
+  result = password == "letmein" ? "Access granted." : "Access denied.";
 
   context.res = {
     // status: 200, /* Defaults to 200 */
