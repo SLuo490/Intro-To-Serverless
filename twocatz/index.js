c318803e61cbb18e584806c38eb7e86dfee7f72c
Shi Tao Luo <sluo5991@gmail.com>
2022-06-14 14:49:12 -0400

    complete week1 step 7

--- twocatz/index.js
+++ twocatz/index.js
@@ -3,21 +3,50 @@ const fetch = require("node-fetch");
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
 
-  const resp = await fetch(
-    "https://bit-cat.azurewebsites.net/cat/says/serverless",
-    {
-      method: "GET",
-    }
-  );
+  async function getCatPic() {
+    let resp = await fetch(
+      "https://bit-cat.azurewebsites.net/cat/says/serverless",
+      {
+        method: "GET",
+      }
+    );
 
-  // we need to receive it as a buffer since this is an image we are receiving from the API
-  const data = await resp.arrayBuffer();
+    // we need to receive it as a buffer since this is an image we are receiving from the API
+    let data = await resp.arrayBuffer();
 
-  //data will be encoded in base64.
-  var base64data = Buffer.from(data).toString("base64");
+    //data will be encoded in base64.
+    var base64data = Buffer.from(data).toString("base64");
+
+    return base64data;
+  }
+
+  function getNames() {
+    var names = [
+      "Shreya",
+      "Emily",
+      "Fifi",
+      "Beau",
+      "Evelyn",
+      "Julia",
+      "Daniel",
+      "Fardeen",
+    ];
+
+    var randomValue = Math.floor(names.length * Math.random());
+    var resultName = names[randomValue];
+    return resultName;
+  }
+
+  var firstCat = await getCatPic();
+  var secondCat = await getCatPic();
+  var name1 = getNames();
+  var name2 = getNames();
 
   context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: { base64data },
+    body: {
+      cat1: firstCat,
+      cat2: secondCat,
+      names: [name1, name2],
+    },
   };
 };
